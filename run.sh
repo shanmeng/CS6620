5688cc596532b3410c597e0d2882ccadfb9714e5
shanmeng <shanmeng.us@gmail.com>
2025-07-25 18:59:40 -0700

    Part 3

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Building API container..."
-docker build -f Dockerfile.api -t packmybag-api .
-
-echo "Running API container on http://localhost:5000 ..."
-docker run -p 5050:5000 packmybag-api
-
-
+echo "Running application stack using docker-compose.yml ..."
+docker-compose up --build
