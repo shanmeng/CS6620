6af2c9cce57145e2076b2583fea8d0849b6a23d5
shanmeng <shanmeng.us@gmail.com>
2025-07-26 19:22:22 -0700

    Part 3 Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Starting test stack using docker-compose.test.yml ..."
+echo "Starting test stack using docker-compose.test.yml..."
 docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit 
 
-echo "Test finished. Cleaning up test stack ..."
+echo "Test finished. Cleaning up test stack..."
 docker-compose -f docker-compose.test.yml down -v
 
-echo "Cleaning up leftover localstack directory ..."
+echo "Cleaning up leftover localstack directory..."
 fuser -k /tmp/localstack || true
 rm -rf /tmp/localstack || true
