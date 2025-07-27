b9248c247edb949592209c0ec6f7efd142a3b929
shanmeng <shanmeng.us@gmail.com>
2025-07-26 18:30:48 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,23 +2,11 @@
 set -e
 
 echo "Starting test stack using docker-compose.test.yml ..."
-docker-compose -f docker-compose.test.yml up --build -d
+docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit 
 
-echo "Waiting for LocalStack service to become healthy..."
-docker-compose -f docker-compose.test.yml wait localstack
-
-echo "LocalStack is ready and healthy!"
-
-echo "Running tests..."
-docker-compose -f docker-compose.test.yml run --rm test
-
-TEST_EXIT_CODE=$?
-
-echo "Cleaning up test stack ..."
+echo "Test finished. Cleaning up test stack ..."
 docker-compose -f docker-compose.test.yml down -v
 
 echo "Cleaning up leftover localstack directory ..."
 fuser -k /tmp/localstack || true
 rm -rf /tmp/localstack || true
-
-exit $TEST_EXIT_CODE
