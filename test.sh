e1e32c85acc714906f55fc0eadd609ad5149d51d
shanmeng <shanmeng.us@gmail.com>
2025-08-05 17:56:13 -0400

    Part 3: Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Starting test stack using docker-compose.test.yml..."
-docker compose -f docker-compose.test.yml up --build --abort-on-container-exit 
+docker compose -f docker-compose.test.yml up --build --abort-on-container-exit
 
 echo "Test finished. Cleaning up test stack..."
 docker compose -f docker-compose.test.yml down -v
